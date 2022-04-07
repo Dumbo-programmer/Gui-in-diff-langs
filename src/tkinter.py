7b9f73ff5932f7358e9390fb7b51d5f744bc03f3
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2022-04-07 18:20:48 +0600

    Create tkinter.py

---
+++ src/tkinter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# declare the window
+window = Tk()
+# set window title
+window.title("Python Tkinter Gui")
+# set window width and height
+window.configure(width=500, height=300)
+# set window background color
+window.configure(bg='lightgray')
+window.mainloop()
