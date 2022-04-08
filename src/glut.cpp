70e4553c2e99ddf94472f81fdb62006f7f993d21
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2022-04-08 21:02:38 +0600

    Create glut.cpp

---
+++ src/glut.cpp
@@ -0,0 +1,11 @@
+#include <GL/glut.h>
+
+int main (int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("My new window");
+    /* ... */
+}
